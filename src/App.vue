2e5c3ae27c73406de7c0a7645e5ed5818f6c653d
tiagogarcia178 <tiagobgarcia@icloud.com>
2022-03-20 20:05:44 +0000

    TheResources component whit tabs to change dynamicaly component, and highlight the selected component in the tab

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <template>
   <TheHeader title="RememberMe" />
-  <StoredResources :resources="storedResources" />
+  <TheResources />
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    StoredResources, //register the Component
     TheHeader,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Oficial Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.com',
-        },
-      ],
-    };
+    TheResources,
   },
 };
 </script>
---
+++ src/components/learning-resources/AddResource.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Add Resource</h1>
+</template>
