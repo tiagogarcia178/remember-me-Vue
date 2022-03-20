92b964e2b5d54e0da9a5e76966cd8590a6936629
tiagogarcia178 <tiagobgarcia@icloud.com>
2022-03-20 00:00:08 +0000

    first project

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <TheHeader title="RememberMe" />
+  <StoredResources :resources="storedResources" />
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  components: {
+    StoredResources, //register the Component
+    TheHeader,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Oficial Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+/* are used in the entire application */
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- this component is registered globally to use everywhere -->
+  <div>
+      <slot></slot> <!-- slot recives HTML, from when this component is used -->
+  </div>
+</template>
+
+<style scoped>
+div {
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  margin: 2rem auto;
+  max-width: 40rem;
+}
+</style>
+
+
+
